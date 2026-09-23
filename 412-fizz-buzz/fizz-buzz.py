329e41d05d0d5c270f2487d0f6cf3fcddbb947dd
Hemant Mohanty <116831187+hemantmohanty@users.noreply.github.com>
2026-09-23 12:03:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (54.52%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> list[str]:
+        ans = []
+
+        for num in range(1, n+1):
+            if num % 3 == 0 and num % 5 == 0:
+                ans.append("FizzBuzz")
+            elif num % 3 == 0:
+                ans.append("Fizz")
+            elif num % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(num))
+        return ans
